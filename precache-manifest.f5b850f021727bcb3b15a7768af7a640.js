9870f2101f7bf365384069e5cc362a47c4e6b925
Simon <simonxd@gmail.com>
2020-05-31 16:55:34 -0300

    Updates

---
+++ precache-manifest.f5b850f021727bcb3b15a7768af7a640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56be9d7696a55120c536bd13e3e40ca0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ede901d2372d7913cd85",
+    "url": "/static/css/main.9e49e6d6.chunk.css"
+  },
+  {
+    "revision": "74b2ee5494a9e0f8c9d2",
+    "url": "/static/js/2.5910d291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5910d291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede901d2372d7913cd85",
+    "url": "/static/js/main.ccf498b3.chunk.js"
+  },
+  {
+    "revision": "56c52dd7ea3ef76789a7",
+    "url": "/static/js/runtime-main.8a397035.js"
+  }
+]);
